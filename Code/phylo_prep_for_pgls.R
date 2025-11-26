e7a3e6ae507e7ba1a42e64bbd5fedb5dd8591b9e
Gustavo Burin <arietebio@gmail.com>
2025-11-26 11:05:51 +0100

    started adding code to extract age of triplets

--- Code/phylo_prep_for_pgls.R
+++ Code/phylo_prep_for_pgls.R
@@ -1,6 +1,9 @@
 library("ape")
 library("phytools")
 library("WorldFlora")
+library("foreach")
+library("doMC")
+
 here::i_am("Code/phylo_prep_for_pgls.R")
 
 tree <- read.tree(here::here("Data/ALLMB.tre"))
@@ -114,3 +117,108 @@ tree.final.base <- drop.tip(tree.base, tree.base$tip.label[!(tree.base$tip.label
 tree.final.base <- drop.tip(tree.final.base, which(duplicated(tree.final.base$tip.label) == TRUE))
 
 write.tree(tree.final.base, here::here("Output/tree_pgls_base_clean.tre"))
+
+
+
+
+### Obtaining divergence times between WR2 and WR1
+
+moltree <- read.tree(here::here("Data/GBOTB.tre"))
+moltree <- multi2di(moltree)
+
+full.data <- read.csv(here::here("Data/20250211_LongFormat_croparchitecture_V3.csv"))
+
+pruned.tree <- drop.tip(moltree, moltree$tip.label[!(moltree$tip.label %in% unique(full.data$Species.ssp.var))])
+
+unique.crops <- unique(full.data$common.name[-grep("wild relative", full.data$common.name)])
+
+triplets <- vector(mode = "list", length = length(unique.crops))
+for(i in 1:length(triplets)){
+    triplets[[i]] <- unique(full.data$Species.ssp.var[grep(unique.crops[i], full.data$common.name)])
+}
+
+registerDoMC(10)
+
+subtrees <- foreach(i = 1:length(triplets)) %dopar%{
+                drop.tip(tree, tree$tip.label[!(tree$tip.label %in% triplets[[i]])])
+            }
+
+sapply(subtrees, Ntip)
+
+crops <- full.data[full.data$wild.cultivated == "Cultivated", ]
+
+crops$Crop <- gsub("[ \t]+$", "", crops$Species.ssp.var, perl = TRUE)
+crops$Crop <- gsub(" ", "_", crops$Crop)
+crops$genus <- sapply(crops$Crop, function(x){strsplit(x, split = "_", fixed = TRUE)[[1]][1]})
+crops$Family <- gsub("[ \t]+$", "", crops$Family, perl = TRUE)
+
+## Fixing families
+## crops$Family[crops$Tree == "Cyrtosperma_merkusii"] <- "Araceae"
+## crops$Family[grep("Breynia", crops$Tree)] <- "Phyllanthaceae"
+## crops$Family[grep("Elaeagnus", crops$Tree)] <- "Elaeagnaceae"
+## crops$Family[crops$Tree == "Elymus_caninus"] <- "Gramineae"
+## crops$Family[crops$Tree == "Myrica_rubra"] <- "Myricaceae"
+## crops$Family[crops$Tree == "Myristica_fragrans"] <- "Myristicaceae"
+## ## Problems with Sapindaceae - Allophylus_cobbe -> could be Rhus_cobbe, Anacardiaceae
+## crops$Family[grep("Vitis", crops$Tree)] <- "Vitaceae"
+
+genera.in.phylo <- lapply(unique(crops$genus), function(x){grep(x, tree$tip.label)})
+genera.not.in.phylo <- unique(crops$genus)[which(sapply(genera.in.phylo, length) == 0)]
+
+## genus.conv <- data.frame(not.phylo = genera.not.in.phylo,
+##                          in.phylo = c("Uvaria",
+##                                       "Apium", ## sister genus
+##                                       "Wahlenbergia",
+##                                       "Polymnia",
+##                                       "Ambrosia", ## sister genus, along with Xanthium
+##                                       "Merremia",
+##                                       "Sedum",
+##                                       "Dupontia", ## sister group to Poa
+##                                       "Bambusa",
+##                                       "Myrciaria",
+##                                       "Oxyria", ## one of sister genera, along with Rheum
+##                                       "Chaenomeles",
+##                                       "Symphonia", ## from sister group Symphonieae
+##                                       "Cardiospermum", ## sister genus
+##                                       "Chrysophyllum",
+##                                       "Musella" ## Sister genus to Musa
+##                                       )
+##                          )                                      
+
+genus.conv <- data.frame(not.phylo = genera.not.in.phylo,
+                         in.phylo = c("Bambusa", ## Synonym to Musa
+                                      "Chrysophyllum", ## Synonym to Gambeya
+                                      "Chaenomeles" ## Synonym to Pseudocydonia
+                                      )
+                         )
+
+tree$tip.label <- gsub("Bambusa_horsfieldii", "Fimbribambusa_horsfieldii", tree$tip.label)
+tree$tip.label <- gsub("Chaenomeles_sinensis", "Pseudocydonia_sinensis", tree$tip.label)
+tree$tip.label <- gsub("Chrysophyllum_africanum", "Gambeya_africana", tree$tip.label)
+tree$tip.label <- gsub("Chrysophyllum_beguei", "Gambeya_beguei", tree$tip.label)
+tree$tip.label <- gsub("Chrysophyllum_boukokoense", "Gambeya_boukokoensis", tree$tip.label)
+
+## crops$genus[!is.na(match(crops$genus, genus.conv$not.phylo))] <- genus.conv$in.phylo[na.omit(match(crops$genus, genus.conv$not.phylo))]
+
+
+## ## Reincluding Vigna_unguiculata after Guillaume's instructions
+
+## crops$Crop[419] <- "Vigna_unguiculata_cowpea"
+## crops$Crop[420] <- "Vigna_unguiculata_asparagus"
+
+tips.to.keep <- sapply(lapply(paste0(sort(unique(crops$genus)), "_"), function(x){grep(x, tree$tip.label, fixed = TRUE)}), function(y){y[1]})
+
+tree.pruned <- drop.tip(tree, tree$tip.label[-tips.to.keep])
+
+tree.gls <- phytools::force.ultrametric(tree.pruned, "nnls")
+tree.gls$node.label <- NULL
+
+for(i in 1:nrow(crops)){
+    print(i)
+    tree.gls <- bind.tip(tree.gls, tip.label = crops$Species.ssp.var[i], where = grep(paste0(crops$genus[i], "_"), tree.gls$tip.label, fixed = TRUE)[1], edge.length = 0)
+}
+
+tree.final.tip <- drop.tip(tree.gls, tree.gls$tip.label[!(tree.gls$tip.label %in% crops$Species.ssp.var)])
+tree.final.tip <- drop.tip(tree.final.tip, which(duplicated(tree.final.tip$tip.label) == TRUE))
+
+write.tree(tree.final.tip, here::here("Output/tree_pgls_tip_clean.tre"))
